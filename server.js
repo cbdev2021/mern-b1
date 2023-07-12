810f8db757208b4368e1175cc28706ca69f6b0a6
carlos <cbdev2021@gmail.com>
2023-07-12 19:57:05 -0400

    cors for netlifly

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
+//const cors = require('cors'); //solucion abajo
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
@@ -13,7 +15,7 @@ connectDB();
 
 const app = express();
 
-const cors = require('cors');
+
 app.use(cors({
   origin: ['https://localhost:3000', 'https://glittery-pasca-60dcc8.netlify.app']
 }));
